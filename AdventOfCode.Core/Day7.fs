bc2ba80d05cca2ccd18ccd608f1086a3de22dd9b
Paul Marfleet <paul.marfleet@gmail.com>
2021-01-24 19:14:07 +0000

    Day 7 Parser WIP

---
+++ AdventOfCode.Core/Day7.fs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode.Core
+
+module Day7 =
+
+    open Farkle
+    open Farkle.Builder
+    open Farkle.Builder.Regex
+
+    type BagColour =
+        | LightRed
+        | DarkOrange
+        | BrightWhite
+        | MutedYellow
+        | ShinyGold
+        | DarkOlive
+        | VibrantPlum
+        | FadedBlue
+        | DottedBlack
+
+    type Content = { Type : BagColour; Units : int }
+
+    type Line = { Type : BagColour; Contents : Content list }
+
+    type Lines = Lines of Line list
+
+    type Day7Parser () =
+
+        static let number = Terminals.genericUnsigned<int> "UnsignedNumber"
+
+        static let colour =
+            [
+                "light red", LightRed
+                "dark orange", DarkOrange
+                "bright white", BrightWhite
+                "muted yellow", MutedYellow
+                "shiny gold", ShinyGold
+                "dark olive", DarkOlive
+                "vibrant plum", VibrantPlum
+                "faded blue", FadedBlue
+                "dotted black", DottedBlack
+            ]
+            |> List.map (fun (name, x) -> !& name =% x)
+            |> (||=) "BagColour"
+
+        static let bagWord = regexString "bag(s)?" |> terminal "BagWord" (T(fun _ data -> data.ToString()))
+
+        static let bagType = "BagType" ||= [
+            !@ colour .>>. bagWord => (fun c _ -> c)
+        ]
+
+        static let contentItem = "ContentItem" ||= [
+            !@ number .>>. bagType => (fun n t -> { Type = t; Units = n })
+        ]
+
+        static let contents =  sepBy1 (literal ",") contentItem
+
+        static let line = "Line" ||= [
+            !@ bagType .>> "contain" .>>. contents .>> "."
+            => (fun t c -> { Type = t; Contents = c })
+        ]
+
+        static let lines = "Lines" ||= [
+            !@ (many1 line) => Lines
+        ]
+
+        static let linesParser = RuntimeFarkle.build lines
+
+        static let bagTypeParser = RuntimeFarkle.build bagType
+
+        static let lineParser = RuntimeFarkle.build line
+
+        static let contentItemParser = RuntimeFarkle.build contentItem
+
+        static let contentsParser = RuntimeFarkle.build contents
+
+        static member private parse input parser =
+            match RuntimeFarkle.parseString parser input with
+            | Ok result -> result
+            | Error err -> failwith (err.ToString())
+
+        static member parseLines input = Day7Parser.parse input linesParser
+
+        static member parseBagType input = Day7Parser.parse input bagTypeParser
+
+        static member parseContentItem input = Day7Parser.parse input contentItemParser 
+
+        static member parseContents input = Day7Parser.parse input contentsParser
+
+        static member parseLine input = Day7Parser.parse input lineParser 
---
+++ AdventOfCode.Tests/Day7.fs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Tests
+
+module Day7 =
+    
+    open Xunit
+    open FsUnit.Xunit
+    open AdventOfCode.Core.Day7
+
+    type ParserTests ()=
+
+        [<Fact>]
+        member _.``Day 7 Parse Bag Type`` () = 
+            Day7Parser.parseBagType "light red bag" |> should equal LightRed
+            Day7Parser.parseBagType "light red bags" |> should equal LightRed
+
+        [<Fact>]
+        member _.``Day 7 Parse Bag Content Item`` () = 
+            Day7Parser.parseContentItem "1 bright white bag" |> should equal { Type = BrightWhite; Units = 1 }
+            Day7Parser.parseContentItem "2 muted yellow bag" |> should equal { Type = MutedYellow; Units = 2 }
+
+        [<Fact>]
+        member _.``Day 7 Parse Bag Contents`` () = 
+            Day7Parser.parseContents "1 bright white bag, 2 muted yellow bags" 
+            |> should equal [ { Type = BrightWhite; Units = 1 }; { Type = MutedYellow; Units = 2 } ]
+
+        [<Fact>]
+        member _.``Day 7 Parse Line`` () = 
+            Day7Parser.parseLine "light red bags contain 1 bright white bag, 2 muted yellow bags." 
+            |> should equal 
+                { Type = LightRed; 
+                  Contents = [ { Type = BrightWhite; Units = 1 }; { Type = MutedYellow; Units = 2 } ] }
